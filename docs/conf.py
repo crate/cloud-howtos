37bbd554be63c1d495d03f279f15de37ef3178e4
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-05-06 16:50:52 +0200

    Prune config (no longer needed)

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,1 @@
 from crate.theme.rtd.conf.cloud_howtos import *
-
-source_suffix = '.rst'
-
-exclude_patterns = ['.*', 'README.rst']
-
-master_doc = 'index'
-
-extensions = ['sphinx_sitemap']
-site_url = 'https://crate.io/docs/cloud/howtos/en/latest/'
-
